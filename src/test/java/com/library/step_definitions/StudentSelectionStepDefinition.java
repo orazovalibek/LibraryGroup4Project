2788630aa2403391242cec5950f466914650e6ad
RitaFurman <margaret_furman@yahoo.com>
2020-11-02 14:25:49 -0600

    adding a student.feature file

---
+++ src/test/java/com/library/step_definitions/StudentSelectionStepDefinition.java
@@ -0,0 +1,28 @@
+package com.library.step_definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StudentSelectionStepDefinition {
+    @Given("I am on Library application login page.")
+    public void iAmOnLibraryApplicationLoginPage() {
+    }
+
+    @And("I login using valid credentials.")
+    public void iLoginUsingValidCredentials() {
+    }
+
+    @And("I click on {string} link.")
+    public void iClickOnLink(String arg0) {
+    }
+
+    @When("I click on {string} drop down menu.")
+    public void iClickOnDropDownMenu(String arg0) {
+    }
+
+    @Then("I should be able to click on {string} selection.")
+    public void iShouldBeAbleToClickOnSelection(String arg0) {
+    }
+}
---
+++ src/test/resources/features/StudentSelection.feature
@@ -0,0 +1,8 @@
+Feature:Select a student
+
+  Scenario: As an authorized user, I should be able to use the filter and select only Students
+    Given I am on Library application login page.
+    And I login using valid credentials.
+    And I click on "Users" link.
+    When I click on "User Group" drop down menu.
+    Then I should be able to click on "Students" selection.
