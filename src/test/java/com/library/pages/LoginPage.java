54a718301813e00a2400ecbd90af6b94212fc3c1
AishaAydin <ayseydin13@gmail.com>
2020-11-02 15:45:10 -0500

    Login page

--- src/test/java/com/library/pages/LoginPage.java
+++ src/test/java/com/library/pages/LoginPage.java
@@ -4,25 +4,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class LoginPage extends BasePage {
-
-    @FindBy(id="inputEmail")
-    public WebElement username;
-
-    @FindBy(id="inputPassword")
-    public WebElement password;
-
-    @FindBy(xpath = "(//span[@class='title'])[2]")
-    public WebElement userModule;
-
-    @FindBy (xpath = "//span[@class='title'])[3]")
-    public WebElement bookModule;
-
-    public void login(){
-        username.sendKeys(ConfigurationReader.getProperty("username"));
-        password.sendKeys(ConfigurationReader.getProperty("password"), Keys.ENTER);
-
-    }
-
+public class LoginPage{
 
 }
